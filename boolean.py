5638068972e869c88697de4ccaf69e6bd5a8a5e0
parthigpm123 <parthigpm123@gmail.com>
2025-06-28 15:20:09 +0530

    new file

---
+++ boolean.py
@@ -0,0 +1,37 @@
+# boolean datatype
+
+b= True
+if  b:
+      
+      print(type(b))
+else:
+       print("not")
+       
+print(10 > 5)
+print(10 !=5)
+print(10 == 5)
+print(10 < 5)       
+
+print(bool())
+
+x=10
+y="python"
+print(bool(x))
+print(bool(y))
+
+a = 10
+b = 20
+if a== b:
+      print("true")
+else:
+      print("false")
+       
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))#
+
