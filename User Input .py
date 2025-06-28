b71a483700051c4b1097a8299ef32f403839ae42
parthigpm123 <parthigpm123@gmail.com>
2025-06-28 17:15:21 +0530

    userinput

---
+++ User Input .py
@@ -0,0 +1,44 @@
+# User input and type casting
+
+num=input()
+print(num + " " + " is awesome")                                    
+
+
+
+
+
+
+num = input()
+print(num*5)
+
+a =int(input())
+print(a + 10)
+
+
+
+num = int(input())
+print(num-5)
+
+
+user=float(input())
+print(type(user))
+
+
+
+'''
+a = ("********************")
+b = ("********************")
+print(a+ " PYTHON IS AWESOME " +b)
+
+
+
+print("""
+      ********************
+      ********************
+       PYTHON IS AWESOME
+      ********************
+      ********************""")
+'''
+
+
+
