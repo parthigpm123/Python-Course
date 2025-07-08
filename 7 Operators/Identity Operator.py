1681e6ca81abea60fbdfc73000eb01b88543197d
parthigpm123 <parthigpm123@gmail.com>
2025-07-09 00:36:44 +0530

    new file

---
+++ 7 Operators/Identity Operator.py
@@ -0,0 +1,10 @@
+#Logical operator find out memory location
+
+
+# 1 is  two object is same memory location  result are true
+
+print(10 is 10)
+
+# 2 is not  memory location are not same location result are false
+
+print(10 is not 10)
--- 7 Operators/Logical operators.py
+++ 7 Operators/Logical operators.py
@@ -6,5 +6,14 @@
 # 3 not
 
 
-
+# 1 and  operators if both condition are only true return true value
 print(10>10 and 20<40)
+
+
+# 2 or any one value are true result will be true
+
+print(10>5 or 10>11)
+
+# 3 not inverse result 
+a = True
+print ( not a)
