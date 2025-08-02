ab479b3fe50c792a86332313403544328fa07741
parthigpm123 <parthigpm123@gmail.com>
2025-08-02 18:29:19 +0530

    new file

--- Tkinter.py/tk.py
+++ Tkinter.py/tk.py
@@ -1,5 +1,24 @@
 import tkinter 
-print(tkinter.TkVersion)
 
-import kivy
-print(kivy.__Version__)
+#tkinter allows to  create GUI applications or widgets
+
+#window
+root= tkinter.Tk()
+root.geometry("750x600")
+root.configure(bg="light blue")
+
+#label
+
+label =tkinter.Label(root, 
+                     text="Hello Python Developers",fg="blue",
+                     bg="sky blue",
+                     font="Arial 50"
+)
+
+label.pack(expand=True, anchor="center")
+
+
+#use pack to position and pack a GUI into a container (window or frame)
+label.pack()
+#Run application
+root.mainloop()
