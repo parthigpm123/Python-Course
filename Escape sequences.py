9af13fb464d67c0f479736cd612586f5cf34a45a
parthigpm123 <parthigpm123@gmail.com>
2025-08-16 14:39:04 +0530

    new file

---
+++ Escape sequences.py
@@ -0,0 +1,20 @@
+'''
+Escape Sequence	Meaning
+\n	New line (line break)
+\t	Horizontal tab
+\\	Backslash (\) itself
+\'	Single quote (')
+\"	Double quote (")
+'''
+
+print("hello \nworld") #new line or line break
+
+
+print("hello\tworld") # horizontal tab (space between words)
+
+
+print("hello\\world  ")
+
+print("hello's world")
+
+print("hello\'s world")
---
+++ Math Module.py
@@ -0,0 +1,20 @@
+import math
+
+a = min(20,30,409)
+
+b= max(20 ,30,50)
+
+print(a,b)
+#------------------------------------------------------
+c=maximum_of_value =math.ceil(5.66) 
+
+d=minimum_of_value= math.floor(7.66)
+
+print(c,d)
+# power function -----------------------------------------------------
+e=math.pow(2, 3)
+
+print(e)
+#-----------------------------------------------------
+
+# absolute value
