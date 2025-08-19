7e6b16875c16d3ed1941be37179705c8b3940aff
parthigpm123 <parthigpm123@gmail.com>
2025-08-19 15:56:00 +0530

    new

--- OS module.py
+++ OS module.py
@@ -1,3 +1,17 @@
+
+'''
+1. File and Directory Management:
+Creating directories: Use os.mkdir() to create a single directory or os.makedirs() to create nested directories. 
+Deleting directories: os.rmdir() removes an empty directory, while os.removedirs() removes nested directories recursively. 
+Renaming files and directories: The os.rename() function can change the name of a file or directory. 
+Listing files and directories: os.listdir() returns a list of files and directories within a specified path. 
+Changing the current working directory: Use os.chdir() to navigate between directories. 
+Getting the current working directory: os.getcwd() returns the path of the current working directory. 
+Checking if a path exists: os.path.exists() verifies the existence of a file or directory. 
+'''
+
+
+
 import os
 #print(dir(os))
 
