f818530706fdedc46304c9002c3cf42c9d989738
parthigpm123 <parthigpm123@gmail.com>
2025-08-29 22:54:22 +0530

    new

---
+++ nested if.py
@@ -0,0 +1,23 @@
+
+num = 10
+
+
+
+if num > 0:
+
+    print("The number is positive.")
+
+    if num % 2 == 0:
+
+        print("The number is also even.")
+
+    else:
+
+        print("The number is odd.")
+
+else:
+
+    print("The number is not positive.")
+    
+    
+    
