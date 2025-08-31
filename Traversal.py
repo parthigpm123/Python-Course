821d62439cc53d83edd16b93014dbb851b413b05
parthigpm123 <parthigpm123@gmail.com>
2025-09-01 00:23:58 +0530

    ternary opp

---
+++ Traversal.py
@@ -0,0 +1,27 @@
+lst =["mango","banana","graphs","orange"]
+ 
+ 
+ 
+ #method 1
+for i in range (len(lst)):
+      
+      print(lst[i])
+      
+print("----------------------------")
+# method 2 
+
+for i in lst:
+      print(i)     
+
+
+
+
+#Slicing
+
+string ="python"
+
+print(string[0:5])      
+
+lst1=[1,2,3,4,5]
+
+print(lst1[1:5])
