5895fd15faebb7bfa97f4a0eb0151b62b883c364
parthigpm123 <parthigpm123@gmail.com>
2025-09-02 11:08:51 +0530

    newfile

--- While loop.py
+++ While loop.py
@@ -9,10 +9,19 @@ i = 1
 while i <10:
   print(i)
   i += 1      
-'''
+
 i = 6 
 while(i>0):
       print(i)
       i=i-1
+'''      
+
+
+count=1 # star value 
+
+while count<10: # end value 
+      
+      print(count,"hi")
+      count+=1 # Step Value
       
       
