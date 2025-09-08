7ca4c98c72dd222cbad04224afa7650a2034df66
parthigpm123 <parthigpm123@gmail.com>
2025-09-08 22:44:40 +0530

    Encapsulation

---
+++ OOPS Concept.py/Abstractions/Abstraction.py
@@ -0,0 +1,4 @@
+'''
+Abstraction in Python, a core principle of Object-Oriented Programming (OOP), involves handling complexity by hiding unnecessary details and exposing only the essential information to the user. It allows developers to focus on what an object does rather than how it achieves its functionality. 
+
+'''
