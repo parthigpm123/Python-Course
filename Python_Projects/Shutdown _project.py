944f19ea21291fe860aa118a2e1a6f13d24e6a7a
parthigpm123 <parthigpm123@gmail.com>
2025-09-11 13:57:07 +0530

    new file

---
+++ Python_Projects/Shutdown _project.py
@@ -0,0 +1,15 @@
+import os
+
+print("1.Shutdown")
+print("2.Restart")
+print("3.Exit")
+
+
+Command =input("Enter Your Command :")
+
+if "1" in Command:
+      os.system("shutdown /s")
+if "2" in Command:
+      os.system("shutdown /r")
+if "3" in Command:
+      exit()           
