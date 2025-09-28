a0b427dd33be91f1a6d52c364b4a13749f919908
parthigpm123 <parthigpm123@gmail.com>
2025-09-28 17:20:17 +0530

    Pstgresql

--- POSTGRESQL/DML/UPDATE.sql
+++ POSTGRESQL/DML/UPDATE.sql
@@ -6,3 +6,5 @@ UPDATE studentrecords SET student_name = 'Parthiban M' WHERE student_id = '1';
 UPDATE studentrecords
 SET student_name = 'Kumar K'
 WHERE student_id = '2';
+
+update myrecords set std_name='M Parthiban' where s_no=1;
