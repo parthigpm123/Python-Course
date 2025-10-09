62b776552bdc34650680b384da735ff65c84ce66
parthigpm123 <parthigpm123@gmail.com>
2025-10-09 23:10:19 +0530

    billing

--- Python_Projects/bill/Billing Software.py
+++ Python_Projects/bill/Billing Software.py
@@ -7,7 +7,7 @@ root.geometry("1270x685")
 #root.iconbitmap("icon.ico")
 
 headingLabel=Label(root,text="Retail Billing System",font=("times new roman",30,"bold"),bg="blue2",fg="gold",bd=12,relief=GROOVE)
-headingLabel.pack(fill=X,pady=10)
+headingLabel.pack(fill=X)
 
 customer_details_frame=LabelFrame(root,text="Customer Details",font=("times new roman",15,"bold"),fg="gold",bd=8,relief=GROOVE,bg="blue2")
 customer_details_frame.pack(fill=X)
@@ -34,40 +34,40 @@ searchButton=Button(customer_details_frame,text='SEARCH',font=('arial',12,'bold'
 searchButton.grid(row=0,column=6,padx=20,pady=8)
 
 productsFrame=Frame(root)
-productsFrame.pack(pady=10)
+productsFrame.pack()
 
 #============COSMETICS====================#
 cosmeticsFrame=LabelFrame(productsFrame,text='Cosmetics',font=('times new roman',15,'bold'),fg='gold',bd=8,relief=GROOVE,bg='blue2')
 cosmeticsFrame.grid(row=0,column=0)
 
 
-bathsoapLabel=Label(cosmeticsFrame,text='Bath Soap',font=("times new roman",15,"bold"),bg="blue2",fg="white")
+bathsoapLabel=Label(cosmeticsFrame,text='Bath Soap',font=("times new roman",13,"bold"),bg="blue2",fg="white")
 bathsoapLabel.grid(row=0,column=0,pady=9,padx=10,sticky='w')
 bathsoapEntry=Entry(cosmeticsFrame,font=("times new roman",15,"bold"),width=10,bd=5)
 bathsoapEntry.grid(row=0,column=1,pady=9,padx=10)
 
 
-facecreamLabel=Label(cosmeticsFrame,text='FaceCream',font=("times new roman",15,"bold"),bg="blue2",fg="white")
+facecreamLabel=Label(cosmeticsFrame,text='FaceCream',font=("times new roman",13,"bold"),bg="blue2",fg="white")
 facecreamLabel.grid(row=1,column=0,pady=9,padx=10,sticky='w')
 facecreamEntry=Entry(cosmeticsFrame,font=("times new roman",15,"bold"),width=10,bd=5)
 facecreamEntry.grid(row=1,column=1,padx=10)
 
-facewashLabel=Label(cosmeticsFrame,text='FaceWash',font=("times new roman",15,"bold"),bg="blue2",fg="white")
+facewashLabel=Label(cosmeticsFrame,text='FaceWash',font=("times new roman",13,"bold"),bg="blue2",fg="white")
 facewashLabel.grid(row=2,column=0,pady=9,padx=10,sticky='w')
 facewashEntry=Entry(cosmeticsFrame,font=("times new roman",15,"bold"),width=10,bd=5)
 facewashEntry.grid(row=2,column=1,padx=10)
 
-hairsprayLabel=Label(cosmeticsFrame,text='HairSpray',font=("times new roman",15,"bold"),bg="blue2",fg="white")
+hairsprayLabel=Label(cosmeticsFrame,text='HairSpray',font=("times new roman",13,"bold"),bg="blue2",fg="white")
 hairsprayLabel.grid(row=3,column=0,pady=9,padx=10,sticky='w')
 hairsprayEntry=Entry(cosmeticsFrame,font=("times new roman",15,"bold"),width=10,bd=5)
 hairsprayEntry.grid(row=3,column=1,padx=10)
 
-hairgelLabel=Label(cosmeticsFrame,text='Hairgel',font=("times new roman",15,"bold"),bg="blue2",fg="white")
+hairgelLabel=Label(cosmeticsFrame,text='Hairgel',font=("times new roman",13,"bold"),bg="blue2",fg="white")
 hairgelLabel.grid(row=4,column=0,pady=9,padx=10,sticky='w')
 hairgelEntry=Entry(cosmeticsFrame,font=("times new roman",15,"bold"),width=10,bd=5)
 hairgelEntry.grid(row=4,column=1,padx=10)
 
-bodyloationLabel=Label(cosmeticsFrame,text='Body Lotion',font=("times new roman",15,"bold"),bg="blue2",fg="white")
+bodyloationLabel=Label(cosmeticsFrame,text='Body Lotion',font=("times new roman",13,"bold"),bg="blue2",fg="white")
 bodyloationLabel.grid(row=5,column=0,pady=9,padx=10,sticky='w')
 bodyloationEntry=Entry(cosmeticsFrame,font=("times new roman",15,"bold"),width=10,bd=5)
 bodyloationEntry.grid(row=5,column=1,padx=10)
@@ -76,32 +76,32 @@ bodyloationEntry.grid(row=5,column=1,padx=10)
 groceryframe=LabelFrame(productsFrame,text='Grocery',font=('times new roman',15,'bold'),fg='gold',bd=8,relief=GROOVE,bg='blue2')
 groceryframe.grid(row=0,column=1)
 
-riceLabel=Label(groceryframe,text='Rice',font=("times new roman",15,"bold"),bg="blue2",fg="white")
+riceLabel=Label(groceryframe,text='Rice',font=("times new roman",13,"bold"),bg="blue2",fg="white")
 riceLabel.grid(row=0,column=0,pady=9,padx=10,sticky='w')
 riceEntry=Entry(groceryframe,font=("times new roman",15,"bold"),width=10,bd=5)
 riceEntry.grid(row=0,column=1,pady=9,padx=10)
 
-oilLabel=Label(groceryframe,text='Oil',font=("times new roman",15,"bold"),bg="blue2",fg="white")
+oilLabel=Label(groceryframe,text='Oil',font=("times new roman",13,"bold"),bg="blue2",fg="white")
 oilLabel.grid(row=1,column=0,pady=9,padx=10,sticky='w')
 oilEntry=Entry(groceryframe,font=("times new roman",15,"bold"),width=10,bd=5)
 oilEntry.grid(row=1,column=1,padx=10)
 
-daalLabel=Label(groceryframe,text='Daal',font=("times new roman",15,"bold"),bg="blue2",fg="white")
+daalLabel=Label(groceryframe,text='Daal',font=("times new roman",13,"bold"),bg="blue2",fg="white")
 daalLabel.grid(row=2,column=0,pady=9,padx=10,sticky='w')
 daalEntry=Entry(groceryframe,font=("times new roman",15,"bold"),width=10,bd=5)
 daalEntry.grid(row=2,column=1,padx=10)
 
-wheatLabel=Label(groceryframe,text='Wheat',font=("times new roman",15,"bold"),bg="blue2",fg="white")
+wheatLabel=Label(groceryframe,text='Wheat',font=("times new roman",13,"bold"),bg="blue2",fg="white")
 wheatLabel.grid(row=3,column=0,pady=9,padx=10,sticky='w')
 wheatEntry=Entry(groceryframe,font=("times new roman",15,"bold"),width=10,bd=5)
 wheatEntry.grid(row=3,column=1,padx=10)
 
-sugarLabel=Label(groceryframe,text='Sugar',font=("times new roman",15,"bold"),bg="blue2",fg="white")
+sugarLabel=Label(groceryframe,text='Sugar',font=("times new roman",13,"bold"),bg="blue2",fg="white")
 sugarLabel.grid(row=4,column=0,pady=9,padx=10,sticky='w')
 sugarEntry=Entry(groceryframe,font=("times new roman",15,"bold"),width=10,bd=5)
 sugarEntry.grid(row=4,column=1,padx=10)
 
-teaLabel=Label(groceryframe,text='Tea',font=("times new roman",15,"bold"),bg="blue2",fg="white")
+teaLabel=Label(groceryframe,text='Tea',font=("times new roman",13,"bold"),bg="blue2",fg="white")
 teaLabel.grid(row=5,column=0,pady=9,padx=10,sticky='w')
 teaEntry=Entry(groceryframe,font=("times new roman",15,"bold"),width=10,bd=5)
 teaEntry.grid(row=5,column=1,padx=10)
@@ -111,32 +111,32 @@ teaEntry.grid(row=5,column=1,padx=10)
 ColdDrinks=LabelFrame(productsFrame,text='Cold Drinks',font=('times new roman',15,'bold'),fg='gold',bd=8,relief=GROOVE,bg='blue2')
 ColdDrinks.grid(row=0,column=2)
 
-maazaLabel=Label(ColdDrinks,text='Maaza',font=("times new roman",15,"bold"),bg="blue2",fg="white")
+maazaLabel=Label(ColdDrinks,text='Maaza',font=("times new roman",13,"bold"),bg="blue2",fg="white")
 maazaLabel.grid(row=0,column=0,pady=9,padx=10,sticky='w')
 maazaEntry=Entry(ColdDrinks,font=("times new roman",15,"bold"),width=10,bd=5)
 maazaEntry.grid(row=0,column=1,pady=9,padx=10)
 
-pepsiLabel=Label(ColdDrinks,text='Pepsi',font=("times new roman",15,"bold"),bg="blue2",fg="white")
+pepsiLabel=Label(ColdDrinks,text='Pepsi',font=("times new roman",13,"bold"),bg="blue2",fg="white")
 pepsiLabel.grid(row=1,column=0,pady=9,padx=10,sticky='w')
 pepsiEntry=Entry(ColdDrinks,font=("times new roman",15,"bold"),width=10,bd=5)
 pepsiEntry.grid(row=1,column=1,padx=10)
 
-spriteLabel=Label(ColdDrinks,text='Sprite',font=("times new roman",15,"bold"),bg="blue2",fg="white")
+spriteLabel=Label(ColdDrinks,text='Sprite',font=("times new roman",13,"bold"),bg="blue2",fg="white")
 spriteLabel.grid(row=2,column=0,pady=9,padx=10,sticky='w')
 spriteEntry=Entry(ColdDrinks,font=("times new roman",15,"bold"),width=10,bd=5)
 spriteEntry.grid(row=2,column=1,padx=10)
 
-dewLabel=Label(ColdDrinks,text='Dew',font=("times new roman",15,"bold"),bg="blue2",fg="white")
+dewLabel=Label(ColdDrinks,text='Dew',font=("times new roman",13,"bold"),bg="blue2",fg="white")
 dewLabel.grid(row=3,column=0,pady=9,padx=10,sticky='w')
 dewEntry=Entry(ColdDrinks,font=("times new roman",15,"bold"),width=10,bd=5)
 dewEntry.grid(row=3,column=1,padx=10)
 
-frootiLabel=Label(ColdDrinks,text='Frooti',font=("times new roman",15,"bold"),bg="blue2",fg="white")
+frootiLabel=Label(ColdDrinks,text='Frooti',font=("times new roman",13,"bold"),bg="blue2",fg="white")
 frootiLabel.grid(row=4,column=0,pady=9,padx=10,sticky='w')
 frootiEntry=Entry(ColdDrinks,font=("times new roman",15,"bold"),width=10,bd=5)
 frootiEntry.grid(row=4,column=1,padx=10)
 
-colaLabel=Label(ColdDrinks,text='Coco cola',font=("times new roman",15,"bold"),bg="blue2",fg="white")
+colaLabel=Label(ColdDrinks,text='Coco cola',font=("times new roman",13,"bold"),bg="blue2",fg="white")
 colaLabel.grid(row=5,column=0,pady=9,padx=10,sticky='w')
 colaEntry=Entry(ColdDrinks,font=("times new roman",15,"bold"),width=10,bd=5)
 colaEntry.grid(row=5,column=1,padx=10)
@@ -152,8 +152,62 @@ billareaLabel.pack(fill=X)
 #=====TEXT AREAR========
 scrollbar=Scrollbar(billframe,orient=VERTICAL)
 scrollbar.pack(side=RIGHT,fill=Y)
-textarea=Text(billframe,height=17,width=55)
+textarea=Text(billframe,height=17,width=55,yscrollcommand=scrollbar.set)
 textarea.pack()      
+scrollbar.config(command=textarea.yview)
 
 
+billmenuFrame=LabelFrame(root,text='Bill Menu',font=('times new roman',15,'bold'),fg='gold',bd=8,relief=GROOVE,bg='blue2')
+billmenuFrame.pack()
+
+cosmeticpriceLabel=Label(billmenuFrame,text='Cosmetic Price',font=("times new roman",13,"bold"),bg="blue2",fg="white")
+cosmeticpriceLabel.grid(row=0,column=0,pady=6,padx=10,sticky='w')
+cosmeticpriceEntry=Entry(billmenuFrame,font=("times new roman",15,"bold"),width=10,bd=5)
+cosmeticpriceEntry.grid(row=0,column=1,pady=6,padx=10)
+
+grocerypriceLabel=Label(billmenuFrame,text='Grocery Price',font=("times new roman",13,"bold"),bg="blue2",fg="white")
+grocerypriceLabel.grid(row=1,column=0,pady=6,padx=10,sticky='w')
+grocerypriceEntry=Entry(billmenuFrame,font=("times new roman",15,"bold"),width=10,bd=5)
+grocerypriceEntry.grid(row=1,column=1,pady=6,padx=10)
+
+drinkspriceLabel=Label(billmenuFrame,text='Cold Drink Price',font=("times new roman",13,"bold"),bg="blue2",fg="white")
+drinkspriceLabel.grid(row=2,column=0,pady=6,padx=10,sticky='w')
+drinkspriceEntry=Entry(billmenuFrame,font=("times new roman",13,"bold"),width=10,bd=5)
+drinkspriceEntry.grid(row=2,column=1,pady=6,padx=10)
+
+#=====================cosmetic tax===========
+cosmetictaxLabel=Label(billmenuFrame,text='Cosmetic Tax',font=("times new roman",13,"bold"),bg="blue2",fg="white")
+cosmetictaxLabel.grid(row=0,column=2,pady=6,padx=10,sticky='w')
+cosmetictaxEntry=Entry(billmenuFrame,font=("times new roman",15,"bold"),width=10,bd=5)
+cosmetictaxEntry.grid(row=0,column=3,pady=6,padx=10)
+
+grocerytaxLabel=Label(billmenuFrame,text='Grocery Tax',font=("times new roman",13,"bold"),bg="blue2",fg="white")
+grocerytaxLabel.grid(row=1,column=2,pady=6,padx=10,sticky='w')
+grocerytaxEntry=Entry(billmenuFrame,font=("times new roman",15,"bold"),width=10,bd=5)
+grocerytaxEntry.grid(row=1,column=3,pady=6,padx=10)
+
+drinkstaxLabel=Label(billmenuFrame,text='Cold Drink Tax',font=("times new roman",13,"bold"),bg="blue2",fg="white")
+drinkstaxLabel.grid(row=2,column=2,pady=6,padx=10,sticky='w')
+drinkstaxEntry=Entry(billmenuFrame,font=("times new roman",13,"bold"),width=10,bd=5)
+drinkstaxEntry.grid(row=2,column=3,pady=6,padx=10)
+
+#================BUTTON FRAME==============
+
+buttonFrame=Frame(billmenuFrame,bd=8,relief=GROOVE)
+buttonFrame.grid(row=0,column=4,rowspan=3)
+
+totalButton=Button(buttonFrame,text="Total",font=('arial',16,"bold"),bg='blue2',fg='white',bd=5,width=8,pady=10)
+totalButton.grid(row=0,column=0,pady=20,padx=5)
+
+billButton=Button(buttonFrame,text="Bill",font=('arial',16,"bold"),bg='blue2',fg='white',bd=5,width=8,pady=10)
+billButton.grid(row=0,column=1,pady=20,padx=5)
+
+emailButton=Button(buttonFrame,text="Email",font=('arial',16,"bold"),bg='blue2',fg='white',bd=5,width=8,pady=10)
+emailButton.grid(row=0,column=2,pady=20,padx=5)
+
+printButton=Button(buttonFrame,text="Print",font=('arial',16,"bold"),bg='blue2',fg='white',bd=5,width=8,pady=10)
+printButton.grid(row=0,column=3,pady=20,padx=5)
+
+clearButton=Button(buttonFrame,text="Clear",font=('arial',16,"bold"),bg='blue2',fg='white',bd=5,width=8,pady=10)
+clearButton.grid(row=0,column=4,pady=20,padx=5)
 root.mainloop() 
