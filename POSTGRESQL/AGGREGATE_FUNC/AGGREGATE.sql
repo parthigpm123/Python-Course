088b25456701503d2dad57c8793a3a52be8bcb5a
parthigpm123 <parthigpm123@gmail.com>
2025-10-17 16:43:10 +0530

    psql

---
+++ POSTGRESQL/AGGREGATE_FUNC/AGGREGATE.sql
@@ -0,0 +1,9 @@
+🔹 Common Aggregate Functions 5
+
+Function	Description	Example:
+
+COUNT()Counts number of rows	SELECT COUNT(*) FROM students;
+SUM()	Adds up values in a column	SELECT SUM(marks) FROM students;
+AVG()	Returns average value	SELECT AVG(marks) FROM students;
+MAX()	Returns highest value	SELECT MAX(marks) FROM students;
+MIN()	Returns lowest value	SELECT MIN(marks) FROM students;
