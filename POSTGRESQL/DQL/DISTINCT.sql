2de259bcdaf0ec2a814e440addb49b1d0f3524da
parthigpm123 <parthigpm123@gmail.com>
2025-10-20 18:46:33 +0530

    psql

---
+++ POSTGRESQL/DQL/DISTINCT.sql
@@ -0,0 +1,6 @@
+/*The DISTINCT keyword in SQL is used with the SELECT statement to retrieve only unique values from a specified column or set of columns in a database table. It eliminates duplicate records from the query's result set. */
+
+
+SELECT DISTINCT firstname FROM employee;
+
+SELECT DISTINCT first_name, last_name FROM employees;
