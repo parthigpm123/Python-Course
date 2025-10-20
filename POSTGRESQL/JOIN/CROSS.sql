fda9db717a43da07e3584a3688cefa6b4e4be48e
parthigpm123 <parthigpm123@gmail.com>
2025-10-20 18:48:01 +0530

    psql

--- POSTGRESQL/JOIN/CROSS.sql
+++ POSTGRESQL/JOIN/CROSS.sql
@@ -1,3 +1,5 @@
 /*CROSS JOIN (Cartesian Product): This is a type of join operation that combines every row from the first table with every row from the second table. The result set contains a number of rows equal to the product of the number of rows in each table. It is used when there is no join condition specified, or when a complete combination of all rows between two datasets is desired.*/
 
+Also called a Cartesian Join.
+
 SELECT * FROM studentrecords CROSS JOIN employee;
