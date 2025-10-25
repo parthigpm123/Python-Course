6928f99fbebf4bdf05929a6ca9e7cecb0dff66c0
parthigpm123 <parthigpm123@gmail.com>
2025-10-25 23:48:08 +0530

    Billing application

---
+++ Identifier.py
@@ -0,0 +1,37 @@
+'''Python Identifiers are user-defined names for variables, functions, or classes. Must follow naming rules (no digits at start, only _ allowed)'''
+'''
+Rules for Python Identifiers:
+lower case-a to z
+
+upper case-A to Z
+
+digits 0 to 9
+
+Underscore _'''
+
+'''
+dont use special characters like @, #, $, %, etc.
+
+dont use reserved keywords like if, else, while, etc.
+
+dont use digits at the start of the identifier
+
+meaningful names are preferred
+
+Examples of valid identifiers:
+
+Area_of_Circle
+Valid identifiers:
+
+var1
+
+_var1
+
+_1_var
+
+var_1
+
+
+Total_Amount
+'''
+
