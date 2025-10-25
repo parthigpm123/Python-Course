004029d549b43b650871cd21b337b1f433345364
parthigpm123 <parthigpm123@gmail.com>
2025-10-25 23:55:56 +0530

    Billing application

--- Keywords.py
+++ Keywords.py
@@ -21,3 +21,8 @@ Scope and Namespace	global, nonlocal
 
 Async Programming	async, await
 '''
+
+import keyword
+print(keyword.kwlist)
+
+''''False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield'''
