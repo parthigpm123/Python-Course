504bf2470ea60cb6cab4704b413ad0ee29f7aa72
parthigpm123 <parthigpm123@gmail.com>
2025-10-26 16:59:57 +0530

    p

--- CONSTANTS/CONSTANTS.py
+++ CONSTANTS/CONSTANTS.py
@@ -1,3 +1,5 @@
 CONSTANTS_WORD = "This is a constant word"
 
 CONN=4
+
+a=10
--- CONSTANTS/main.py
+++ CONSTANTS/main.py
@@ -1,5 +1,9 @@
+'''In Python, constants are variables whose values are intended to remain unchanged throughout a program. They are typically defined using uppercase letters to signify their fixed nature, often with words separated by underscores (e.g., MAX_LIMIT).'''
+
 import CONSTANTS as c
 
 print(c.CONSTANTS_WORD)
 
 print(c.CONN)
+
+print(c.a)
