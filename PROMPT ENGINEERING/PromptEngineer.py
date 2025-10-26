d5a79092359559c9f962bace140b12f2a6bb96a8
parthigpm123 <parthigpm123@gmail.com>
2025-10-26 20:57:12 +0530

    p

--- PROMPT ENGINEERING/PromptEngineer.py
+++ PROMPT ENGINEERING/PromptEngineer.py
@@ -1,2 +1,18 @@
 '''What is Prompt Engineering?
 Prompt engineering is the process of creating effective prompts that enable AI models to generate responses based on given inputs. Prompt engineering essentially means writing prompts intelligently for text-based Artificial Intelligence tasks, more specifically, Natural Language Processing (NLP) tasks. In the case of such text-based tasks, these prompts help the user and the model generate a particular output as per the requirement. These requirements are efficiently added in the form of prompts and hence the name Prompt Engineering. '''
+
+'''
+MAIN TOPICS IN PROMPT ENGINEERING:
+     
+ 1.TASK DEFINITION
+ 
+ 2.CONTEXT SETTING
+ 
+ 3.PERSONA DESIGNING
+ 
+ 4.EXAMPLAR GENERATION
+ 
+ 5.PROMPT FORMATTING 
+ 
+ 6.TONE    
+ '''
