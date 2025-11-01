dd9a5d40041519f9676afdd05db75da551483216
parthigpm123 <parthigpm123@gmail.com>
2025-11-01 09:00:31 +0530

    Billing application

--- Python Strings/title.py
+++ Python Strings/title.py
@@ -1,3 +1,8 @@
 #title method - Each Word's First Letter Capitalized
 
 print('hello world python!'.title())
+
+
+a='hello world python! welcome to python strings'
+r=a.replace('python','java')
+print(r)
